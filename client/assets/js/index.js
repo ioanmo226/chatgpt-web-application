550f4f517352aecc2dd243fb1ab54f4fbc9c5a06
Ioan Moldovan <mobiletar108@outlook.com>
2023-01-27 12:12:22 -0400

    feat: added loader

--- client/assets/js/index.js
+++ client/assets/js/index.js
@@ -5,6 +5,8 @@ const promptInput = document.getElementById("prompt-input");
 const responseList = document.getElementById("response-list");
 let isGeneratingResponse = false;
 
+let loadInterval = null;
+
 promptInput.addEventListener("keydown", function(event) {
     if (event.key === 'Enter') {
         event.preventDefault();
@@ -16,17 +18,42 @@ promptInput.addEventListener("keydown", function(event) {
     }
 });
 
+function generateUniqueId() {
+    // Used to generate unique IDs
+    const timestamp = Date.now();
+    const randomNumber = Math.random();
+    const hexadecimalString = randomNumber.toString(16);
+
+    return `id-${timestamp}-${hexadecimalString}`;
+}
+
 function addResponse(selfFlag, prompt) {
+    const uniqueId = generateUniqueId();
     const html = `
             <div class="response-container ${selfFlag ? 'my-question' : 'chatgpt-response'}">
                 <img src="assets/img/${selfFlag ? 'me' : 'chatgpt'}.png" alt="avatar"/>
-                <pre><code class="prompt-content">${prompt}</code></pre>
+                <pre><code class="prompt-content" id="${uniqueId}">${prompt}</code></pre>
             </div>
         `
     responseList.insertAdjacentHTML('beforeend', html);
-    hljs.highlightAll()
+    if (prompt) {
+        hljs.highlightAll()
+    }
+    return uniqueId;
 }
+function loader(element) {
+    element.textContent = ''
 
+    loadInterval = setInterval(() => {
+        // Update the text content of the loading indicator
+        element.textContent += '.';
+
+        // If the loading indicator has reached three dots, reset it
+        if (element.textContent === '....') {
+            element.textContent = '';
+        }
+    }, 300);
+}
 function getGPTResult() {
     if (isGeneratingResponse) {
         return;
@@ -36,16 +63,21 @@ function getGPTResult() {
 
     promptInput.textContent = '';
     addResponse(true, prompt);
+    const uniqueId = addResponse(false);
+    loader(document.getElementById(uniqueId));
     isGeneratingResponse = true;
     fetch(`${API_URL}/get-prompt-result?prompt=` + encodeURI(prompt))
         .then(response => response.text())
         .then(responseText => {
-            addResponse(false, responseText);
+            document.getElementById(uniqueId).textContent = responseText.replace(/^\s+/g, '');
+            hljs.highlightAll();
+            responseList.scrollTop = responseList.scrollHeight;
         }).catch((err) => {
         alert(err);
     }).finally(() => {
         isGeneratingResponse = false;
         submitButton.classList.remove("loading");
+        clearInterval(loadInterval);
     });
 }
 
